f856942b4ed8eff05fc26b950082f080c66c475c
John <johnscott.osborne@gmail.com>
2022-05-05 16:47:55 +0100

    added css

--- src/App.js
+++ src/App.js
@@ -2,8 +2,39 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div className="calculator-grid">
+      <div className="calculator-screen">
+        <div className="previousOperand">
+          12,345 /
+        </div>
+        <div className="currentOperand">
+          67,890
+        </div>
+      </div>
+        <button className="span-2">AC</button> 
+        <button>DEL</button>
+        <button>+</button>
+
+        <button>9</button>
+        <button>8</button>
+        <button>7</button>
+        <button>-</button>
+
+        <button>6</button>
+        <button>5</button>
+        <button>4</button>
+        <button>/</button>
+
+        <button>3</button>
+        <button>2</button>
+        <button>1</button>
+        <button>x</button>
+
+        <button>.</button>
+        <button>0</button>
+        <button className="span-2">=</button>
+
+
     </div>
   );
 }
